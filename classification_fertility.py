06c2fbd2ba7d0be8d2091a27eeb8bb975399db06
Hartaj <hartaj.ca@gmail.com>
2026-06-16 19:37:56 +0200

    Added different models to operate the task (Logistic Regression, Support Vector Machine, Random Forest and Multilayer Perceptron)

--- classification_fertility.py
+++ classification_fertility.py
@@ -36,3 +36,49 @@ n_features = X_train_scaled.shape[1]  # mi serve per decidere la griglia di rice
 
 print('\n Numero di training sample =', X_train_scaled.shape[0])
 print('\n Numero di feature =', n_features)
+
+
+#modelli
+
+# contrasto lo sbilanciamento delle classi usando class_weight='balanced'; random_state a 0 uniforma dati randomici
+models = [LogisticRegression(class_weight='balanced', random_state=0),
+          SVC(class_weight='balanced', random_state=0),
+          RandomForestClassifier(class_weight='balanced', random_state=0),      
+          MLPClassifier(random_state=0)] #MLP non ha balanced come parametro
+
+models_names = ['Logistic Regression',
+                'SVM',
+                'Random Forest',
+                'MLP']
+
+models_hparametes = [{'penalty': ['l1', 'l2'], 'C': [1e-5, 5e-5, 1e-4, 5e-4, 1]},           # Log Reg ("C" è il peso della regolarizzazione) ho notato facendo prove che seleziona il piu piccolo tra 'C': [1e-5, 5e-5, 1e-4, 5e-4, 1]
+                     {'C': [0.1, 1, 10, 100], 'gamma': ['scale', 'auto', 0.01, 0.1], 'kernel': ['linear', 'rbf']}, # SVM ('scale' e 'auto' sono adattive alla scala dei dati, come da documentazione scikit) , C in range [1e-4, 1e-2, 1, 1e1, 1e2], noto che seleziona sempre uno nonostante provi con i parametri [0.1, 1, 10, 100]
+                     {'n_estimators': [50, 100, 150], 'max_depth': [3, 5, 7, None], 'min_samples_split': [2, 5]},  # Random Forest, seleziona alberi per dataset piccolo, 7 depth per questo caso e 2 min split, facendo alberi dettagliati
+                     {'hidden_layer_sizes': [n_features, math.floor(n_features/2), n_features*2], \
+                      'alpha': [0.0001, 0.001, 0.01], 'learning_rate_init': [0.001, 0.01, 0.1]}                    # MLP (un solo hidden layer per dataset piccoli, se no rischio overfitting), leaning rate 0.0001 eliminato, rallenta solo il processo
+                     ]
+
+
+trained_models = []
+validation_performance = []
+
+for model, model_name, hparameters in zip(models, models_names, models_hparametes):
+    print('\n ', model_name)
+    clf = GridSearchCV(estimator=model, param_grid=hparameters, scoring='balanced_accuracy', cv=5)
+    clf.fit(X_train_scaled, y_train)
+    trained_models.append((model_name, clf.best_estimator_))
+    print('I valori migliori degli iper-parametri sono:  ', clf.best_params_)
+    print('Accuracy:  ', clf.best_score_)
+    validation_performance.append(clf.best_score_)
+
+
+
+
+#Scelta finale del modello
+
+print('\n..................................................................................')
+best_model_index = np.argmax(validation_performance)
+final_model = trained_models[best_model_index][1]
+print('Ho scelto come miglior modello : ', trained_models[best_model_index][0])
+print('\n I cui iper-parametri sono: ', final_model.get_params())
+print('\n..................................................................................')
